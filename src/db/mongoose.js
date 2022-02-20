ad8570c03978f251850049b69e5f7c75b118e0e9
Mayank0022 <mayank0038.cse19@chitkara.edu.in>
2022-02-20 11:52:15 +0530

    update mongoose

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,11 +2,6 @@ const mongoose = require('mongoose')
 
 mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
-<<<<<<< HEAD
-   // useCreateIndex: true,
-   // useFindAndModify: false
-=======
   //  useCreateIndex: true,
   //  useFindAndModify: false
->>>>>>> 999eb2d93405958b0e7d6c5443282f14b31f32a8
 })
