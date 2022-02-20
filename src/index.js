707b05e97440b3cb4f9d3e642ab7adf98ca13285
Mayank0022 <mayank0038.cse19@chitkara.edu.in>
2022-02-20 13:36:14 +0530

    update

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 
 const app = express()
-const PORT = process.env.PORT||3000
+const PORT = 
 
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT||3000, () => {
     console.log('Server is up on port ' + port)
 })
